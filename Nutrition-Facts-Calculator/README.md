ed9d99146c64b9642b286508966396ea0060e0d4
Security Poodle <123969558+securitypoodle@users.noreply.github.com>
2024-10-15 14:49:41 -0500

    Upload Nutrition-Facts-Calculator and README

---
+++ Nutrition-Facts-Calculator/README.md
@@ -0,0 +1,33 @@
+# Nutrition Facts Calculator
+This application calculates nutrition information for any recipe. 
+
+It takes user input for an ingredient, and then asks the user to provide nutritional information for that ingredient. Once all ingredients are entered, the user provides the desired serving size, and then the app provides the recipe's nutritional information per serving.
+
+## Current Nutritional Information Calculated
+- Fat
+- Sodium
+- Sugar
+- Fiber
+- Protein
+
+## Assumptions and Limitations
+Currently, this app assumes the user knows the **TOTAL** value of each ingredient's nutrients for the recipe. For example, if the recipe calls for `16oz creamy peanut butter`, the user will need to know the total amount of Fat  in 16oz of their peanut butter - meaning, you may have to look at your can and calculate how many servings you're adding to your recipe. 
+
+In this example (as seen in the screenshot below) our jar of peanut butter is 16oz total. The values in the "Nutrition Facts" table show "per serving". We take the **Total Fat** value of 16g, and multiply it by the "**X** servings per container", and get **Fat = 224g**, the number we add into the calculator. 
+
+*\*NOTE: These values may alarm you (e.g. "224g Fat for peanut butter), but remember, we divide the total number by serving size at the end, so the final numbers should be closer to expected values!*
+
+# Example Recipe: Chocolate Covered Peanut Butter Protein Bars
+## Ingredients
+- 2 1/4 cups of Rolled Oats (blended into flour)
+- 1/2 cup Vanilla Protein Powder
+- Salt (to taste)
+- 16oz Creamy Peanut Butter
+- 1/2 cup Honey
+- 1 Tablespoon Vanilla Extract
+- 1/2 cup Mini Chocolate Chips
+- 3x3.5oz Dark Chocolate Bar
+- 1 Teaspoon Coconut Oil
+
+Serving Size = 32 bars (pieces)
+
---
+++ Nutrition-Facts-Calculator/nutrition-facts.py
@@ -0,0 +1,57 @@
+def get_recipe_and_nutrient_data():
+    recipe = {}
+    print('Enter an ingredient, and then its nutrient information.\n')
+    print('Enter "done" when you have listed all ingredients.\n')
+
+    while True:
+        ingredient = input('Enter an ingredient (or "done" to finish): ').strip().lower()
+        if ingredient == 'done':
+            break
+
+        fat = float(input(f'  Enter fat (g) for "{ingredient}": '))
+        sodium = float(input(f'  Enter sodium (mg) for "{ingredient}": '))
+        sugar = float(input(f'  Enter sugar (g) for "{ingredient}": '))
+        fiber = float(input(f'  Enter fiber (g) for "{ingredient}": '))
+        protein = float(input(f'  Enter protein (g) for "{ingredient}": '))
+
+        recipe[ingredient] = {
+            'nutrition': {
+                'Fat (g)': fat,
+                'Sodium (mg)': sodium,
+                'Sugar (g)': sugar,
+                'Fiber (g)': fiber,
+                'Protein (g)': protein
+            }   
+        }
+
+    return recipe
+
+
+def calc_nutritional_info(recipe):
+    total_nutrients = {'Fat (g)': 0, 'Sodium (mg)': 0, 'Sugar (g)': 0, 'Fiber (g)': 0, 'Protein (g)': 0}
+
+    for ingredient in recipe.values():
+        if ingredient in recipe.values():
+            for nutrient in total_nutrients:
+                total_nutrients[nutrient] += ingredient['nutrition'][nutrient]
+    
+    return total_nutrients
+
+
+def run_calculation():
+    print('--- Nutritional Information Calculator ---')
+
+    recipe = get_recipe_and_nutrient_data()
+    total_nutrients = calc_nutritional_info(recipe)
+
+    serving_size = float(input(f'Enter serving size (in number of pieces): '))
+
+    print('##################')
+    print('##################')
+    print(f'Total Nutrition Facts per serving ({serving_size} pieces): ')
+    for nutrient, value in total_nutrients.items():
+        print(f'- {nutrient.capitalize()}: {value/serving_size:.2f}')
+    print('##################')
+    print('##################')
+
+run_calculation()
